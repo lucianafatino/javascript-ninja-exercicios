7581e88c271dc6bdc3dd01c5254c0f15cf5c9b85
Luciana Fatino Venâncio <lucianavenancio54@gmail.com>
2022-05-16 14:50:51 -0300

    Exercícios resolvidos

--- desafio-01/exercicio-04.js
+++ desafio-01/exercicio-04.js
@@ -1,5 +1,5 @@
 // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
-var comida = ['arroz', 'feijão', 'ovo']; //correto
+var comida = ['arroz','feijão','ovo']; //correto
 
 // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
 comida[1]; //correto
