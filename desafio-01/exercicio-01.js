87153b5808ae5590408097abac61d8d71fffedcf
Luciana Fatino Venâncio <lucianavenancio54@gmail.com>
2022-05-16 14:54:23 -0300

    Exercícios resolvidos

--- desafio-01/exercicio-01.js
+++ desafio-01/exercicio-01.js
@@ -1,12 +1,13 @@
 // Declarar uma variável chamada `myvar`, sem valor.
 
-var myvar; // correto
+var myVar; // correto
 
 // Após declarada, atribua o valor 10 à variável `myvar`.
-myvar = 10; // correto
+myVar = 10; // correto
 
 // Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
-soma === myvar // correto
+var somaNumeros = 15 + 8;
+somaNumeros === myVar // correto
 
 // Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
-myvar <= soma; // correto
+myvar <= somaNumeros; // correto
