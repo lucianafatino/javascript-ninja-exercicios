4a0d53c2da275d125c2c69be9a7ce8077011b77d
Luciana Fatino Venâncio <lucianavenancio54@gmail.com>
2022-05-16 14:52:52 -0300

    Exercícios resolvidos

--- desafio-01/exercicio-02.js
+++ desafio-01/exercicio-02.js
@@ -1,11 +1,11 @@
 // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
-var soma = 15 + 8; // correto
+var somaNumeros = 15 + 8; // correto
 
 // Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado
-soma++; // correto
+somaNumeros++; // correto
 
 // Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
-soma *= 3; // correto
+somaNumeros *= 3; // correto
 
 // Qual é o valor da variável `soma` até aqui?
 72 // correto
