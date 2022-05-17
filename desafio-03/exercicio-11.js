4c6d0b4c05735504f4faa4299b0290c8a98275a4
Luciana Fatino Venâncio <lucianavenancio54@gmail.com>
2022-05-17 09:16:28 -0300

    Exercícios por resolver

---
+++ desafio-03/exercicio-11.js
@@ -0,0 +1,12 @@
+/*
+Declarar uma variável `pessoa`, que receba suas informações pessoais.
+As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
+- `nome` - String
+- `sobrenome` - String
+- `sexo` - String
+- `idade` - Number
+- `altura` - Number
+- `peso` - Number
+- `andando` - Boolean - recebe "falso" por padrão
+- `caminhouQuantosMetros` - Number - recebe "zero" por padrão
+*/
