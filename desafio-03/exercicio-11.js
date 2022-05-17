58272f6c74dedac05ff354070b5f565cfa484bdf
Luciana Fatino Venâncio <lucianavenancio54@gmail.com>
2022-05-17 09:20:51 -0300

    Exercícios por resolver

--- desafio-03/exercicio-11.js
+++ desafio-03/exercicio-11.js
@@ -62,3 +62,11 @@ Agora vamos brincar um pouco com o objeto criado:
 Qual o nome completo da pessoa? (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
+
+
+/*
+Qual a idade da pessoa? (Use a instrução para responder e comentários
+inline ao lado da instrução para mostrar qual foi a resposta retornada)
+*/
+
+
