436b08edd2295d62b6011a8f863ab1d7a2a3d4f4
Luciana Fatino Venâncio <lucianavenancio54@gmail.com>
2022-05-18 18:21:53 -0300

    Exercício feito, sem correção

--- desafio-04/exercicio-12.js
+++ desafio-04/exercicio-12.js
@@ -5,6 +5,14 @@ equivalente booleano para o valor passado no argumento for `true`, ou `false`
 para o contrário.
 */
 
+var isTruthy = function(a){
+    if(a == true){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 // Invoque a função criada acima, passando todos os tipos de valores `falsy`.
 
