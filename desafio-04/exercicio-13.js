e788d96281f3af0d0ee9005d981b36a0c10083fb
Luciana Fatino Venâncio <lucianavenancio54@gmail.com>
2022-05-18 14:51:09 -0300

    Exercícios para realizar

---
+++ desafio-04/exercicio-13.js
@@ -0,0 +1,12 @@
+/*
+Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
+seguintes propriedades (os valores devem ser do tipo mostrado abaixo):
+- `marca` - String
+- `modelo` - String
+- `placa` - String
+- `ano` - Number
+- `cor` - String
+- `quantasPortas` - Number
+- `assentos` - Number - cinco por padrão
+- `quantidadePessoas` - Number - zero por padrão
+*/
