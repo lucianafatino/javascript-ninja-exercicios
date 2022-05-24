c6f26b1990452b8243892af225e1e66ab6262dc1
lucianafatino <lucianavenancio54@gmail.com>
2022-05-24 14:27:23 -0300

    Exercícios para resolver

--- desafio-05/exercicio-14.js
+++ desafio-05/exercicio-14.js
@@ -2,3 +2,12 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
+
+/*
+Crie uma função que receba um array como parâmetro, e retorne esse array.
+*/
+
+/*
+Imprima o segundo índice do array retornado pela função criada acima.
+*/
+
