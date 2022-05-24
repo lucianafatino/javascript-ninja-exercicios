57c298884e4716f30d6d3ac41e1a97188cd31d85
lucianafatino <lucianavenancio54@gmail.com>
2022-05-24 14:27:47 -0300

    Exercícios para resolver

--- desafio-05/exercicio-14.js
+++ desafio-05/exercicio-14.js
@@ -11,3 +11,10 @@ Crie uma função que receba um array como parâmetro, e retorne esse array.
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 
+/*
+Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
+segundo, um número. A função deve retornar o valor de um índice do array que foi passado
+no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
+segundo parâmetro.
+*/
+
