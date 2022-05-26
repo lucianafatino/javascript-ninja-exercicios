04b7cb3988511be6ea5c9904943cd81d65bf58fb
lucianafatino <lucianavenancio54@gmail.com>
2022-05-26 10:51:35 -0300

    Exercícios para realizar

--- desafio-06/exercicio-17.js
+++ desafio-06/exercicio-17.js
@@ -5,3 +5,13 @@ Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
 // ?
+
+/*
+Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
+Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
+estar na ordem em que eles aparecem na tabela no momento da solução desse
+desafio.
+*/
+// ?
+
+console.log( 'Times que estão participando do campeonato:', teams );
