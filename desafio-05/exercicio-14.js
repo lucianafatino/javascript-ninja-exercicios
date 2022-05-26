91407dfe616c588d9105c56d41b8abbd13ceca7e
lucianafatino <lucianavenancio54@gmail.com>
2022-05-26 09:33:23 -0300

    Exercícios resolvidos, aguardando correção

--- desafio-05/exercicio-14.js
+++ desafio-05/exercicio-14.js
@@ -18,16 +18,7 @@ Imprima o segundo índice do array retornado pela função criada acima.
 
 funcArr(arr)[2];
 
-/*
-Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
-segundo, um número. A função deve retornar o valor de um índice do array que foi passado
-no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
-segundo parâmetro.
-*/
 
-function myFunc(arg1, arg2){
-    return arg1[arg2];
-}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
---
+++ desafio-05/exercicio-15.js
@@ -0,0 +1,10 @@
+/*
+Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
+segundo, um número. A função deve retornar o valor de um índice do array que foi passado
+no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
+segundo parâmetro.
+*/
+
+function myFunc(arg1, arg2){
+    return arg1[arg2];
+}
