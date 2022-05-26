7c04326fdc50e04c76d866764bee590faa185ac9
lucianafatino <lucianavenancio54@gmail.com>
2022-05-26 10:50:56 -0300

    Exercícios para realizar

---
+++ desafio-06/exercicio-17.js
@@ -0,0 +1,7 @@
+/*
+Vamos falar um pouco sobre "Futebol". Escolha um campeonato estadual qualquer
+para começar o desafio.
+Declare uma variável chamada `championship` que receberá o nome do campeonato,
+e imprima o nome desse campeonato no console.
+*/
+// ?
