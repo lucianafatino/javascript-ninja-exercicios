59186691ef3e3640e508a4e57296db019106292a
lucianafatino <lucianavenancio54@gmail.com>
2022-05-26 10:51:58 -0300

    Exercícios para realizar

--- desafio-06/exercicio-17.js
+++ desafio-06/exercicio-17.js
@@ -15,3 +15,21 @@ desafio.
 // ?
 
 console.log( 'Times que estão participando do campeonato:', teams );
+
+/*
+Crie uma função chamada `showTeamPosition` com as seguintes características:
+    - A função deve receber um número por parâmetro;
+    - A função deve retornar a frase:
+    "O time que está em [POSIÇÃO]º lugar é o [NOME DO TIME].";
+    - Onde [POSIÇÃO] é o valor passado por parâmetro e [NOME DO TIME] é o time
+    que está nessa posição no array criado acima com os nomes dos times.
+    --------------
+    Dica: lembre-se que arrays começam no índice zero, então a posição passada
+    deve ser sempre um número a mais que o índice do array ;)
+    --------------
+    - A função só deve retornar a frase acima somente se o time estiver entre
+    os 5 primeiros.
+    - Se não houver time para a posição passada, deve retornar a mensagem:
+    "Não temos a informação do time que está nessa posição."
+*/
+// ?
