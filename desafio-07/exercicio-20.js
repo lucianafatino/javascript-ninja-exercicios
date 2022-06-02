faebaeeebbaf7ea64145b57c8bfb9d921683271e
lucianafatino <lucianavenancio54@gmail.com>
2022-06-02 08:59:44 -0300

    Novos exercícios; Aguardando resolução

--- desafio-07/exercicio-20.js
+++ desafio-07/exercicio-20.js
@@ -2,3 +2,10 @@
 Crie um array com 5 items (tipos variados).
 */
 // ?
+
+
+/*
+Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
+A função deverá retornar o array atualizado.
+*/
+// ?
