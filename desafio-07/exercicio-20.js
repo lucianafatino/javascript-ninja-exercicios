0256671329049a0ed15c39d6475c70f6241399c8
lucianafatino <lucianavenancio54@gmail.com>
2022-06-02 09:00:20 -0300

    Novos exercícios; Aguardando resolução

--- desafio-07/exercicio-20.js
+++ desafio-07/exercicio-20.js
@@ -9,3 +9,10 @@ Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 // ?
+
+
+/*
+Adicione um novo array ao array criado no início do desafio, com ao menos 3
+itens de tipos diferentes, mostrando o resultado no console.
+*/
+// ?
