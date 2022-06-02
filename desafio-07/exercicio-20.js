8f599cbda7e96468ff5ec649516af609e0bb9f2b
lucianafatino <lucianavenancio54@gmail.com>
2022-06-02 09:01:01 -0300

    Novos exercícios; Aguardando resolução

--- desafio-07/exercicio-20.js
+++ desafio-07/exercicio-20.js
@@ -16,3 +16,22 @@ Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 // ?
+
+/*
+Mostre no console o segundo elemento desse último array, criado acima, com a
+frase:
+"O segundo elemento do segundo array é [ELEMENTO]."
+*/
+// ?
+
+/*
+Mostre no console quantos itens tem o primeiro array criado, com a frase:
+"O primeiro array tem [QUANTIDADE DE ITENS] itens."
+*/
+// ?
+
+/*
+Agora mostre no console quantos itens tem o segundo array criado, com a frase:
+"O segundo array tem [QUANTIDADE DE ITENS] itens."
+*/
+// ?
