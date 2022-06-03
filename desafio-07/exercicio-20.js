dd81db50760e851aeee345fb8ef8040ada5c7e5c
lucianafatino <lucianavenancio54@gmail.com>
2022-06-03 07:51:42 -0300

    Exercícios resolvidos

--- desafio-07/exercicio-20.js
+++ desafio-07/exercicio-20.js
@@ -17,10 +17,7 @@ function addItem(item){
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
-
-console.log(newArr.push([undefined, -90, 'Luciana']));
-
+newArr([undefined, -90, 'Luciana']); // correto
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
