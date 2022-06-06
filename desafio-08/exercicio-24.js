c21d766b6816fb017507b980023a4ce8b897ac13
lucianafatino <lucianavenancio54@gmail.com>
2022-06-06 08:29:37 -0300

    Exercícios para fazer

---
+++ desafio-08/exercicio-24.js
@@ -0,0 +1,13 @@
+/*
+Crie uma função literal chamada `calculator`, que funcione assim:
+- A função deve receber um parâmetro que dirá qual operação matemática ela
+vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
+- Essa função deve retornar uma segunda função que fará o seguinte:
+  - Essa segunda função deve receber dois parâmetros;
+  - Esses dois parâmetros serão os operandos usados na operação matemática;
+  - O retorno dessa segunda função é a operação matemática completa, com a frase:
+  "Resultado da operação: [NUMERO1] [OPERADOR] [NUMERO2] = [RESULTADO]."
+  - Se o operador não for válido, retornar a frase:
+  "Operação inválida."
+*/
+// ?
