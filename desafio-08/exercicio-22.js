5250be81796eec453d86339a70de020c727d1f4e
lucianafatino <lucianavenancio54@gmail.com>
2022-06-06 08:37:08 -0300

    Exercício parcialmente feito

--- desafio-08/exercicio-22.js
+++ desafio-08/exercicio-22.js
@@ -12,7 +12,7 @@ Invoque a função criada acima, passando dois números que serão somados, e mo
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+console.log(`A soma entre 2 e 3 é igual a ${sum(2,3)}`)
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
