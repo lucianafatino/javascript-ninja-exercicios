d7fcb9a39e92a0ed25ea3a8fa2819027581653aa
lucianafatino <lucianavenancio54@gmail.com>
2022-06-06 08:28:47 -0300

    Exercícios para fazer

--- desafio-08/exercicio-23.js
+++ desafio-08/exercicio-23.js
@@ -8,3 +8,10 @@ seu nome.
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
 // ?
+
+/*
+Usando a variável criada acima, mostre no console o nome e o retorno da função
+atribuída a ela, com a seguinte frase:
+"A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
+*/
+// ?
