bf663613b571c646d5fcfbff962313d325c3214c
lucianafatino <lucianavenancio54@gmail.com>
2022-06-06 08:28:24 -0300

    Exercícios para fazer

---
+++ desafio-08/exercicio-23.js
@@ -0,0 +1,10 @@
+/*
+Crie uma função literal chamada `showName`. Essa função deve retornar o
+seu nome.
+*/
+// ?
+
+/*
+Declare uma variável chamada `varShowName` que recebe a função criada acima.
+*/
+// ?
