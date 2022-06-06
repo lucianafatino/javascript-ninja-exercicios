61246a86b3ace4def2f01d7bf2b25f710fb736c9
lucianafatino <lucianavenancio54@gmail.com>
2022-06-06 19:21:32 -0300

    Exemplos práticos das aulas

--- desafio-08/exercicio-23.js
+++ desafio-08/exercicio-23.js
@@ -2,16 +2,18 @@
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName(){
+    return(`Luciana Fatino Venâncio`);
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+var varShowName = showName();
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log(`A função ${showName.name} retorna ${varShowName}`);
